84df14c579f676affc7e598d1c2b3436f40cea18
Dominik Pilar <xpilar@fi.muni.cz>
2021-09-20 09:56:05 +0200

    Added no args constructor to DefinitionLevel class.

--- src/main/java/cz/muni/ics/kypo/training/feedback/dto/resolver/DefinitionLevel.java
+++ src/main/java/cz/muni/ics/kypo/training/feedback/dto/resolver/DefinitionLevel.java
@@ -3,11 +3,13 @@ package cz.muni.ics.kypo.training.feedback.dto.resolver;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DefinitionLevel {
 
     @JsonProperty("reference_solution")
